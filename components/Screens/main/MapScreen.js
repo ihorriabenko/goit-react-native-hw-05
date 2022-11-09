c8bfbe0f8b3fe2968637577f24b313927c141121
Ihor Riabenko <100082550+iryabenkoff@users.noreply.github.com>
2022-11-09 12:35:43 +0200

    Initial commit

---
+++ components/Screens/main/MapScreen.js
@@ -0,0 +1,17 @@
+import { StyleSheet, View, Text } from "react-native";
+
+export const Map = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Map</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
---
+++ components/Screens/main/PostsScreen.js
@@ -0,0 +1,17 @@
+import { StyleSheet, View, Text } from "react-native";
+
+export const PostsScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text>PostsScreen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
